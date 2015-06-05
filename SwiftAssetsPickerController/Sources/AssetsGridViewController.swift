6c0e311ad9bcca7311c01f3f4c671ea42cfb6179
Maxim Bilan <maximb.mail@gmail.com>
2015-06-05 18:11:58 +0300

    Modified controllers

--- SwiftAssetsPickerController/Sources/AssetsGridViewController.swift
+++ SwiftAssetsPickerController/Sources/AssetsGridViewController.swift
@@ -6,4 +6,19 @@
 //  Copyright (c) 2015 Maxim Bilan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AssetsGridViewController: UICollectionViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+}
