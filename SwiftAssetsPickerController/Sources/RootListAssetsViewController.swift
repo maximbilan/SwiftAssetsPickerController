fd09950815e2c15b72c6fe5045e57a3405501ce9
Maxim Bilan <maximb.mail@gmail.com>
2015-06-05 18:06:01 +0300

    Added controller templates

---
+++ SwiftAssetsPickerController/Sources/RootListAssetsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RootListAssetsViewController.swift
+//  SwiftAssetsPickerController
+//
+//  Created by Maxim Bilan on 6/5/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import Foundation
