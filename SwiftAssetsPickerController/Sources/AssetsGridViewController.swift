37fcf8d759adcb4903a6caf72d36b33e02747b13
Maxim Bilan <maximb.mail@gmail.com>
2015-06-07 20:09:52 +0300

    Some test changes

--- SwiftAssetsPickerController/Sources/AssetsGridViewController.swift
+++ SwiftAssetsPickerController/Sources/AssetsGridViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import Photos
 
-class AssetsGridViewController: UICollectionViewController {
+class AssetsGridViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 	
 	var collection: PHAssetCollection?
+	var imagesCache: Array<Dictionary<Int, UIImage>> = Array()
+	
 	private let reuseIdentifier = "AssetsGridCell"
 	
 	private var assetsFetchResult: PHFetchResult!
@@ -29,7 +31,7 @@ class AssetsGridViewController: UICollectionViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 		
 		let flowLayout = UICollectionViewFlowLayout()
-		flowLayout.itemSize = CGSizeMake(200, 140)
+		//flowLayout.itemSize = CGSizeMake(200, 140)
 		flowLayout.scrollDirection = UICollectionViewScrollDirection.Vertical
 		
 		collectionView?.collectionViewLayout = flowLayout
@@ -37,7 +39,6 @@ class AssetsGridViewController: UICollectionViewController {
 		collectionView?.registerClass(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: reuseIdentifier)
 		
 		assetsFetchResult = (collection == nil) ? PHAsset.fetchAssetsWithMediaType(.Image, options: nil) : PHAsset.fetchAssetsInAssetCollection(collection, options: nil)
-		
 	}
 	
 	override func didReceiveMemoryWarning() {
@@ -56,24 +57,48 @@ class AssetsGridViewController: UICollectionViewController {
 	
 	override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! UICollectionViewCell
-		//cell.backgroundColor = UIColor.blackColor()
+		cell.backgroundColor = UIColor.blackColor()
 		// Configure the cell
 		
-		let asset = assetsFetchResult[indexPath.row] as! PHAsset
-		
-		if let imageView = cell.viewWithTag(101) {
-			
-		}
-		else {
-			let imageView = UIImageView(frame: cell.frame)
-			imageView.tag = 101
-			PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: CGSizeMake(100, 100), contentMode: PHImageContentMode.AspectFill, options: nil, resultHandler: { (image: UIImage!, info: [NSObject : AnyObject]!) -> Void in
-				imageView.image = image
-			})
-			cell.addSubview(imageView)
-		}
+//		let asset = assetsFetchResult[indexPath.row] as! PHAsset
+//		
+//		if let imageView = cell.viewWithTag(101) as? UIImageView {
+//			for imageCache in imagesCache {
+//				if let image = imageCache[indexPath.row] {
+//					imageView.image = image
+//				}
+//			}
+//		}
+//		else {
+//			let imageView = UIImageView(frame: cell.frame)
+//			imageView.tag = 101
+//			PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: CGSizeMake(100, 100), contentMode: PHImageContentMode.AspectFill, options: nil, resultHandler: { (image: UIImage!, info: [NSObject : AnyObject]!) -> Void in
+//				self.imagesCache.append([indexPath.row: image])
+//				imageView.image = image
+//			})
+//			cell.addSubview(imageView)
+//		}
 		
 		return cell
+	}
+	
+	// MARK: UICollectionViewDelegateFlowLayout
+	
+	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+		let a = (self.view.frame.size.width - 4 * 1 - 2 * 2) * 0.25
+		return CGSizeMake(a, a)
+	}
+	
+	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+		return UIEdgeInsetsMake(2, 2, 2, 2)
+	}
+	
+	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+		return 1
+	}
+	
+	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+		return 1
 	}
 	
 }
