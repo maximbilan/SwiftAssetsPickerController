51542fe79c540bad6554ca0a258a037e39b5775c
Maxim Bilan <maximb.mail@gmail.com>
2016-10-02 11:55:51 +0300

    Updated pods

--- Podfile
+++ Podfile
@@ -3,6 +3,6 @@ use_frameworks!
 
 target "SwiftAssetsPickerController" do
 
-pod 'CheckMarkView', '~> 0.2.4'
+pod 'CheckMarkView', '~> 0.3.0'
 
 end
--- Pods/CheckMarkView/CheckMarkView/Sources/CheckMarkView.swift
+++ Pods/CheckMarkView/CheckMarkView/Sources/CheckMarkView.swift
@@ -39,110 +39,112 @@ public class CheckMarkView: UIView {
 	private var _checked: Bool = false
 	private var _style: CheckMarkStyle = .Nothing
 	
-    override public func drawRect(rect: CGRect) {
-		super.drawRect(rect)
+    override public func draw(_ rect: CGRect) {
+		super.draw(rect)
 		
 		if _checked {
-			drawRectChecked(rect)
+			drawRectChecked(rect: rect)
 		}
 		else {
 			if _style == .OpenCircle {
-				drawRectOpenCircle(rect)
+				drawRectOpenCircle(rect: rect)
 			}
 			else if _style == .GrayedOut {
-				drawRectGrayedOut(rect)
+				drawRectGrayedOut(rect: rect)
 			}
 		}
     }
 	
 	func drawRectChecked(rect: CGRect) {
-		let context = UIGraphicsGetCurrentContext()
+		let context = UIGraphicsGetCurrentContext()!
 		let frame = self.bounds
 		
 		let checkmarkBlue = UIColor(red: 0.078, green: 0.435, blue: 0.875, alpha: 1)
-		let shadow = UIColor.blackColor()
-		let shadowOffset = CGSizeMake(0.1, -0.1)
+		let shadow = UIColor.black
+		let shadowOffset = CGSize(width: 0.1, height: -0.1)
 		let shadowBlurRadius: CGFloat = 2.5
 		
-		let group = CGRectMake(CGRectGetMinX(frame) + 3, CGRectGetMinY(frame) + 3, CGRectGetWidth(frame) - 6, CGRectGetHeight(frame) - 6)
-		let checkedOvalPath = UIBezierPath(ovalInRect: CGRectMake(CGRectGetMinX(group) + floor(CGRectGetWidth(group) * 0.00000 + 0.5), CGRectGetMinY(group) + floor(CGRectGetHeight(group) * 0.00000 + 0.5), floor(CGRectGetWidth(group) * 1.00000 + 0.5) - floor(CGRectGetWidth(group) * 0.00000 + 0.5), floor(CGRectGetHeight(group) * 1.00000 + 0.5) - floor(CGRectGetHeight(group) * 0.00000 + 0.5)))
+		let group = CGRect(x: frame.minX + 3, y: frame.minY + 3, width: frame.width - 6, height: frame.height - 6)
 		
-		CGContextSaveGState(context!)
-		CGContextSetShadowWithColor(context!, shadowOffset, shadowBlurRadius, shadow.CGColor)
+		let checkedOvalPath = UIBezierPath(ovalIn: CGRect(x: group.minX + floor(group.width * 0.00000 + 0.5), y: group.minY + floor(group.height * 0.00000 + 0.5), width: floor(group.width * 1.00000 + 0.5) - floor(group.width * 0.00000 + 0.5), height: floor(group.height * 1.00000 + 0.5) - floor(group.height * 0.00000 + 0.5)))
+		
+		context.saveGState()
+		context.setShadow(offset: shadowOffset, blur: shadowBlurRadius, color: shadow.cgColor)
 		checkmarkBlue.setFill()
 		checkedOvalPath.fill()
-		CGContextRestoreGState(context!)
+		context.restoreGState()
 		
-		UIColor.whiteColor().setStroke()
+		UIColor.white.setStroke()
 		checkedOvalPath.lineWidth = 1
 		checkedOvalPath.stroke()
-		
+
 		let bezierPath = UIBezierPath()
-		bezierPath.moveToPoint(CGPointMake(CGRectGetMinX(group) + 0.27083 * CGRectGetWidth(group), CGRectGetMinY(group) + 0.54167 * CGRectGetHeight(group)))
-		bezierPath.addLineToPoint(CGPointMake(CGRectGetMinX(group) + 0.41667 * CGRectGetWidth(group), CGRectGetMinY(group) + 0.68750 * CGRectGetHeight(group)))
-		bezierPath.addLineToPoint(CGPointMake(CGRectGetMinX(group) + 0.75000 * CGRectGetWidth(group), CGRectGetMinY(group) + 0.35417 * CGRectGetHeight(group)))
-		bezierPath.lineCapStyle = CGLineCap.Square
+		bezierPath.move(to: CGPoint(x: group.minX + 0.27083 * group.width, y: group.minY + 0.54167 * group.height))
+		bezierPath.addLine(to: CGPoint(x: group.minX + 0.41667 * group.width, y: group.minY + 0.68750 * group.height))
+		bezierPath.addLine(to: CGPoint(x: group.minX + 0.75000 * group.width, y: group.minY + 0.35417 * group.height))
+		bezierPath.lineCapStyle = CGLineCap.square
 		
-		UIColor.whiteColor().setStroke()
+		UIColor.white.setStroke()
 		bezierPath.lineWidth = 1.3
 		bezierPath.stroke()
 	}
 	
 	func drawRectOpenCircle(rect: CGRect) {
-		let context = UIGraphicsGetCurrentContext()
+		let context = UIGraphicsGetCurrentContext()!
 		let frame = self.bounds
 		
-		let shadow = UIColor.blackColor()
-		let shadowOffset = CGSizeMake(0.1, -0.1)
+		let shadow = UIColor.black
+		let shadowOffset = CGSize(width: 0.1, height: -0.1)
 		let shadowBlurRadius: CGFloat = 0.5
-		let shadow2 = UIColor.blackColor()
-		let shadow2Offset = CGSizeMake(0.1, -0.1)
+		let shadow2 = UIColor.black
+		let shadow2Offset = CGSize(width: 0.1, height: -0.1)
 		let shadow2BlurRadius: CGFloat = 2.5
 		
-		let group = CGRectMake(CGRectGetMinX(frame) + 3, CGRectGetMinY(frame) + 3, CGRectGetWidth(frame) - 6, CGRectGetHeight(frame) - 6)
-		let emptyOvalPath = UIBezierPath(ovalInRect: CGRectMake(CGRectGetMinX(group) + floor(CGRectGetWidth(group) * 0.00000 + 0.5), CGRectGetMinY(group) + floor(CGRectGetHeight(group) * 0.00000 + 0.5), floor(CGRectGetWidth(group) * 1.00000 + 0.5) - floor(CGRectGetWidth(group) * 0.00000 + 0.5), floor(CGRectGetHeight(group) * 1.00000 + 0.5) - floor(CGRectGetHeight(group) * 0.00000 + 0.5)))
+		let group = CGRect(x: frame.minX + 3, y: frame.minY + 3, width: frame.width - 6, height: frame.height - 6)
+		let emptyOvalPath = UIBezierPath(ovalIn: CGRect(x: group.minX + floor(group.width * 0.00000 + 0.5), y: group.minY + floor(group.height * 0.00000 + 0.5), width: floor(group.width * 1.00000 + 0.5) - floor(group.width * 0.00000 + 0.5), height: floor(group.height * 1.00000 + 0.5) - floor(group.height * 0.00000 + 0.5)))
 		
-		CGContextSaveGState(context!)
-		CGContextSetShadowWithColor(context!, shadow2Offset, shadow2BlurRadius, shadow2.CGColor)
-		CGContextRestoreGState(context!)
+		context.saveGState()
+		context.setShadow(offset: shadow2Offset, blur: shadow2BlurRadius, color: shadow2.cgColor)
+		context.restoreGState()
 		
-		CGContextSaveGState(context!)
-		CGContextSetShadowWithColor(context!, shadowOffset, shadowBlurRadius, shadow.CGColor)
-		UIColor.whiteColor().setStroke()
+		context.saveGState()
+		context.setShadow(offset: shadowOffset, blur: shadowBlurRadius, color: shadow.cgColor)
+		UIColor.white.setStroke()
 		emptyOvalPath.lineWidth = 1
 		emptyOvalPath.stroke()
-		CGContextRestoreGState(context!)
+		context.restoreGState()
 	}
 	
 	func drawRectGrayedOut(rect: CGRect) {
-		let context = UIGraphicsGetCurrentContext()
+		let context = UIGraphicsGetCurrentContext()!
 		let frame = self.bounds
 		
 		let grayTranslucent = UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
-		let shadow = UIColor.blackColor()
-		let shadowOffset = CGSizeMake(0.1, -0.1)
+		let shadow = UIColor.black
+		let shadowOffset = CGSize(width: 0.1, height: -0.1)
 		let shadowBlurRadius: CGFloat = 2.5
 		
-		let group = CGRectMake(CGRectGetMinX(frame) + 3, CGRectGetMinY(frame) + 3, CGRectGetWidth(frame) - 6, CGRectGetHeight(frame) - 6)
-		let uncheckedOvalPath = UIBezierPath(ovalInRect: CGRectMake(CGRectGetMinX(group) + floor(CGRectGetWidth(group) * 0.00000 + 0.5), CGRectGetMinY(group) + floor(CGRectGetHeight(group) * 0.00000 + 0.5), floor(CGRectGetWidth(group) * 1.00000 + 0.5) - floor(CGRectGetWidth(group) * 0.00000 + 0.5), floor(CGRectGetHeight(group) * 1.00000 + 0.5) - floor(CGRectGetHeight(group) * 0.00000 + 0.5)))
+		let group = CGRect(x: frame.minX + 3, y: frame.minY + 3, width: frame.width - 6, height: frame.height - 6)
 		
-		CGContextSaveGState(context!)
-		CGContextSetShadowWithColor(context!, shadowOffset, shadowBlurRadius, shadow.CGColor)
+		let uncheckedOvalPath = UIBezierPath(ovalIn: CGRect(x: group.minX + floor(group.width * 0.00000 + 0.5), y: group.minY + floor(group.height * 0.00000 + 0.5), width: floor(group.width * 1.00000 + 0.5) - floor(group.width * 0.00000 + 0.5), height: floor(group.height * 1.00000 + 0.5) - floor(group.height * 0.00000 + 0.5)))
+		
+		context.saveGState()
+		context.setShadow(offset: shadowOffset, blur: shadowBlurRadius, color: shadow.cgColor)
 		grayTranslucent.setFill()
 		uncheckedOvalPath.fill()
-		CGContextRestoreGState(context!)
+		context.restoreGState()
 		
-		UIColor.whiteColor().setStroke()
+		UIColor.white.setStroke()
 		uncheckedOvalPath.lineWidth = 1
 		uncheckedOvalPath.stroke()
 		
 		let bezierPath = UIBezierPath()
-		bezierPath.moveToPoint(CGPointMake(CGRectGetMinX(group) + 0.27083 * CGRectGetWidth(group), CGRectGetMinY(group) + 0.54167 * CGRectGetHeight(group)))
-		bezierPath.addLineToPoint(CGPointMake(CGRectGetMinX(group) + 0.41667 * CGRectGetWidth(group), CGRectGetMinY(group) + 0.68750 * CGRectGetHeight(group)))
-		bezierPath.addLineToPoint(CGPointMake(CGRectGetMinX(group) + 0.75000 * CGRectGetWidth(group), CGRectGetMinY(group) + 0.35417 * CGRectGetHeight(group)))
-		bezierPath.lineCapStyle = CGLineCap.Square
+		bezierPath.move(to: CGPoint(x: group.minX + 0.27083 * group.width, y: group.minY + 0.54167 * group.height))
+		bezierPath.addLine(to: CGPoint(x: group.minX + 0.41667 * group.width, y: group.minY + 0.68750 * group.height))
+		bezierPath.addLine(to: CGPoint(x: group.minX + 0.75000 * group.width, y: group.minY + 0.35417 * group.height))
+		bezierPath.lineCapStyle = CGLineCap.square
 		
-		UIColor.whiteColor().setStroke()
+		UIColor.white.setStroke()
 		bezierPath.lineWidth = 1.3
 		bezierPath.stroke()
 	}
